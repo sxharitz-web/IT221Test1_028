09a315cac30adeac890f0e415396edf01aab0e7f
sxharitz-web <sxharitz@gmail.com>
2026-02-20 15:26:08 +0700

    Change database password from '1234' to '1111'

--- counter.php
+++ counter.php
@@ -1,5 +1,5 @@
 <?php
-    $link = mysqli_connect("localhost","root","1234");
+    $link = mysqli_connect("localhost","root","1111");
     mysqli_set_charset($link,'utf8');
     mysqli_query($link,"Use room;");
     $sql = "select * from counter;";
